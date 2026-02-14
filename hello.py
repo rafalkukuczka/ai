a030e1be779dfa974160ced542918646b4fe167d
rafal.antoni.kukuczka <rafal.antoni.kukuczka@googlemail.com>
2026-02-14 13:07:56 +0100

    Basic py operations

--- hello.py
+++ hello.py
@@ -1,6 +1,24 @@
+import pandas as pd
+
+data = {
+    "wiek": [25, 40, 30],
+    "pensja": [4000, 8000, 5000],
+    "kupil": [0, 1, 0]
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df["wiek"])
+print(df[["wiek", "pensja"]])
+
+
+
 ages=[30,40,60]
 
 for age in ages:
     if age>50:
         print("Ryzyko:", age)
-        
+
+print(type([1,2,3]), [1,2,3])
+print(type({}))
+print(type(set()))
